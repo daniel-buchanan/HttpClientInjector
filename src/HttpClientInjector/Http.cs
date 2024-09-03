4d2995f9731b6b9cacb06dfe086e50c808bba32b
Daniel Buchanan <daniel.buchanan@rezare.co.nz>
2024-09-04 09:55:57 +1200

    feat: multiple binding

--- src/HttpClientInjector/Http.cs
+++ src/HttpClientInjector/Http.cs
@@ -8,8 +8,13 @@ public interface IHttp<T>
     public HttpClient Client { get; }
 }
 
+public interface IHttp<TInterface, TImplementation> : IHttp<TInterface>;
+
 public class Http<T>(HttpClient client) : IHttp<T>
 {
     public string TypeName { get; } = typeof(T).Name;
     public HttpClient Client { get; } = client;
 }
+
+public class Http<TInterface, TImplementation>(HttpClient client) : 
+    Http<TInterface>(client), IHttp<TInterface, TImplementation>;
--- src/HttpClientInjector/ServiceCollectionExtensions.cs
+++ src/HttpClientInjector/ServiceCollectionExtensions.cs
@@ -34,6 +34,24 @@ namespace HttpClientInjector
             
             return self;
         }
+
+        public static IServiceCollection InjectHttpClientFor<TInterface, TImplementation>(
+            this IServiceCollection self,
+            Action<IHttpClientConfigurationBuilder> builder)
+        {
+            if (self.All(s => s.ServiceType != typeof(IHttpClientFactory)))
+                self.AddHttpClient();
+            
+            var name = GetHttpClientName<TInterface>();
+            self.AddHttpClient<IHttp<TInterface, TImplementation>, Http<TInterface, TImplementation>>(name, (provider, client) =>
+            {
+                var internalBuilder = new HttpClientConfigurationBuilder(provider);
+                builder(internalBuilder);
+                internalBuilder.Apply(client);
+            });
+            
+            return self;
+        }
         
         public static IServiceCollection InjectHttpClientFor<T>(
             this IServiceCollection self, 
@@ -42,6 +60,14 @@ namespace HttpClientInjector
             => self.InjectHttpClientFor<T>(b 
                 => b.WithBaseUrl(baseUrl).WithoutAuthentication());
 
+        public static IServiceCollection InjectHttpClientFor<TInterface, TImplementation>(
+            this IServiceCollection self, 
+            string baseUrl)
+            where TInterface : class
+            where TImplementation : class
+            => self.InjectHttpClientFor<TInterface, TImplementation>(b 
+                => b.WithBaseUrl(baseUrl).WithoutAuthentication());
+        
         public static IServiceCollection InjectHttpClientFor<T>(
             this IServiceCollection self,
             string baseUrl,
@@ -53,12 +79,34 @@ namespace HttpClientInjector
                     builder(b);
                 }));
         
+        public static IServiceCollection InjectHttpClientFor<TInterface, TImplementation>(
+            this IServiceCollection self, 
+            string baseUrl,
+            Action<IHttpClientConfigurationBuilder> builder)
+            where TInterface : class
+            where TImplementation : class
+            => self.InjectHttpClientFor<TInterface, TImplementation>(b =>
+            {
+                b.WithBaseUrl(baseUrl);
+                builder(b);
+            });
+        
         public static IServiceCollection InjectHttpClientFor<T>(
             this IServiceCollection self, 
             string baseUrl, 
             Func<IServiceProvider, IBearerAuthentication> getBearerToken)
             where T : class 
             => self.InjectHttpClientFor<T>(b 
+                => b.WithBaseUrl(baseUrl)
+                    .WithBearerAuthentication(getBearerToken));
+        
+        public static IServiceCollection InjectHttpClientFor<TInterface, TImplementation>(
+            this IServiceCollection self, 
+            string baseUrl, 
+            Func<IServiceProvider, IBearerAuthentication> getBearerToken)
+            where TInterface : class
+            where TImplementation : class
+            => self.InjectHttpClientFor<TInterface, TImplementation>(b 
                 => b.WithBaseUrl(baseUrl)
                     .WithBearerAuthentication(getBearerToken));
         
@@ -70,9 +118,24 @@ namespace HttpClientInjector
             => self.InjectHttpClientFor<T>(b 
                 => b.WithBaseUrl(baseUrl)
                     .WithBasicAuthentication(getBasicCredentials));
+        
+        public static IServiceCollection InjectHttpClientFor<TInterface, TImplementation>(
+            this IServiceCollection self, 
+            string baseUrl, 
+            Func<IServiceProvider, IBasicAuthentication> getBasicCredentials)
+            where TInterface : class
+            where TImplementation : class
+            => self.InjectHttpClientFor<TInterface, TImplementation>(b 
+                => b.WithBaseUrl(baseUrl)
+                    .WithBasicAuthentication(getBasicCredentials));
 
         public static IHttp<T> GetHttpFor<T>(this IServiceProvider provider)
             where T : class
             => provider.GetService<IHttp<T>>();
+        
+        public static IHttp<TInterface, TImplementation> GetHttpFor<TInterface, TImplementation>(this IServiceProvider provider)
+            where TInterface : class
+            where TImplementation : class
+            => provider.GetService<IHttp<TInterface, TImplementation>>();
     }
 }
